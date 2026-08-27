30c0fd3a32242b4ab473594c309fe0ec978d0187
Renan Lira <renanlira.dev@gmail.com>
2026-08-27 15:29:04 -0300

    Starting footer

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
     return (
@@ -44,6 +45,10 @@ export function App() {
                         </DefaultButton>
                     </div>
                 </form>
+            </Container>
+
+            <Container>
+                <Footer />
             </Container>
         </>
     );
