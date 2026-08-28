39358e58a9fbb2fa9b1fbbe639b8d2806f57536d
Renan Lira <renanlira.dev@gmail.com>
2026-08-28 14:06:59 -0300

    Adding theme change

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -2,11 +2,11 @@ import styles from './styles.module.css';
 
 type MenuItemProps = {
     children: React.ReactNode
-}
+} & React.ComponentProps<'a'>
 
-export function MenuItem({ children }: MenuItemProps) {
+export function MenuItem({ children, ...props }: MenuItemProps) {
     return (
-        <a href='#' className={styles['menu-item-link']}>
+        <a href='#' className={styles['menu-item-link']} {...props}>
             {children}
         </a>
     );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,37 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 import { MenuItem } from './MenuItem';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+    function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+        event.preventDefault();
+
+        setTheme((prevTheme) => {
+            return prevTheme === 'dark' ? 'light' : 'dark';
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+    }, [theme]);
+
     return (
         <nav className={styles.menu}>
-            <MenuItem>
+            <MenuItem aria-label='Go to Home' title='Go to Home'>
                 <HouseIcon />
             </MenuItem>
-            <MenuItem>
+            <MenuItem aria-label='Go to History' title='Go to History'>
                 <HistoryIcon />
             </MenuItem>
-            <MenuItem>
+            <MenuItem aria-label='Go to Settings' title='Go to Settings'>
                 <SettingsIcon />
             </MenuItem>
-            <MenuItem>
+            <MenuItem aria-label='Change theme' title='Change theme' onClick={(event) => handleThemeChange(event)}>
                 <SunIcon />
             </MenuItem>
         </nav>
